40d64eeb9b1bbb27814d73815fd02b40c767a609
William-La <wla@g.hmc.edu>
2019-07-24 10:46:00 -0700

    complete '/restaurants' endpoint

--- vagrant/views.py
+++ vagrant/views.py
@@ -28,11 +28,30 @@ app = Flask(__name__)
 
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
-  #YOUR CODE HERE
-    
+  if request.method == 'GET':
+    res = session.query(Restaurant).all()
+    return jsonify(r.serialize for r in res)
+
+  elif request.method == 'POST':
+    meal = request.args.get('mealType', '')
+    loc = request.args.get('location', '')
+    resInfo = findARestaurant(meal, loc)
+    if resInfo != 'none':
+      res = Restaurant(restaurant_name = unicode(restaurant_info['name']),
+      restaurant_address = unicode(restaurant_info['address']),
+      restaurant_image = restaurant_info['image'])
+      session.add(res)
+      session.commit()
+      return jsonify(restaurant = res.serialize)
+
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
-  #YOUR CODE HERE
+  if request.method == 'GET':
+
+  elif request.method == 'PUT':
+
+  elif request.method == 'DELETE':
+
 
 if __name__ == '__main__':
     app.debug = True
